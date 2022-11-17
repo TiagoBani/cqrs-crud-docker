04c20a8d52dc8d7ac9bd9c5bd0a88ea5ad61fe8d
tiagobani <tiago.bani@yahoo.com.br>
2022-11-17 03:58:01 -0300

    build: add initial infra

---
+++ command/src/test/kotlin/com/tiagobani/crqscmmand/CrqsCmmandApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tiagobani.crqscmmand
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrqsCmmandApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ command/src/test/kotlin/com/tiagobani/crqscommand/CrqsCommandApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tiagobani.crqscommand
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrqsCommandApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
